d880eb0f769a5696b9a86a88c302e436be64ebb7
gsarti <gabriele.sarti996@gmail.com>
2019-01-18 11:29:15 +0100

    Last lecture notes

--- lectures/c++/11_mixing/01_symbols/01_hello.c
+++ lectures/c++/11_mixing/01_symbols/01_hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// Compilation steps by default:
+// source file --- preprocess --> assembly file --- assembler --> machine code
+// Linking is language-agnostic. This means code coming from different languages
+// can be linked together.
+//
+
 int main() {
   printf("hello, world\n");
   return 0;
